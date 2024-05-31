14d459482be9ee0b833e14cbd6803da96f89030f
Anna DI PLACIDO <anna.di-placido@etu.unice.fr>
2024-05-31 19:47:50 +0200

    lien render

--- src/game.js
+++ src/game.js
@@ -6,7 +6,9 @@ import Football from "./gamesTypes/football";
 const Colyseus = require('colyseus.js');
 
 
-const client = new Colyseus.Client('ws://localhost:2567');
+//const client = new Colyseus.Client('ws://localhost:2567');
+const client = new Colyseus.Client('wss://serveroatjo.onrender.com');
+
 
 let canvas;
 let engine;
